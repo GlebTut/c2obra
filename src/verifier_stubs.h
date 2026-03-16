5a6b3c30ef58f0421d6e88eb7ab3a38a7e63732c
GlebTut <glebtutubalin@gmail.com>
2026-03-16 18:39:27 +0000

    feat: add verifier_stubs.h forward declarations

---
+++ src/verifier_stubs.h
@@ -0,0 +1,8 @@
+#ifndef VERIFIER_STUBS_H
+#define VERIFIER_STUBS_H
+extern void reach_error(void);
+extern void __VERIFIER_error(void);
+extern void __VERIFIER_assume(int);
+extern int  __VERIFIER_nondet_int(void);
+extern int  __VERIFIER_nondet_bool(void);
+#endif
